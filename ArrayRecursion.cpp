e74d72157d7c940d83c745bf341918a1abfac5c4
Anshu Tingre <anshu.tingre2129@gmail.com>
2025-12-18 12:17:27 +0530

    Recursion on Array

---
+++ ArrayRecursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+// Using Recursion to check if the array is sorted or Not.
+bool isSorted(vector<int> arr , int n){
+    if(n==0 || n == 1){                  //Base Condition 
+        return true;
+    }
+
+    return arr[n-1] >= arr[n-2] && isSorted(arr,n-1); 
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4,5};
+
+    cout<<isSorted(arr,arr.size());
+}
